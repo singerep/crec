e91aed3e8fef8129881fb274e735b90c1dc2c45c
Ethan Singer <ethansinger101@gmail.com>
2023-06-01 11:33:19 -0400

    switched validation to api.py

--- crec/api.py
+++ crec/api.py
@@ -1,4 +1,50 @@
-class GovInfoAPI:
-    def __init__(self, api_key=None) -> None:
+import httpx
+import time
+from typing import Union
+import asyncio
+from collections import defaultdict
+
+
+class GovInfoAPIError(BaseException):
+    pass
+
+
+class OverRateLimit(BaseException):
+    pass
+
+
+class OverRetryLimit(BaseException):
+    pass
+
+
+class GovInfoAPI(httpx.AsyncClient):
+    def __init__(self, wait: Union[bool, int] = 300, retry_limit: Union[bool, int] = 5):
+        super().__init__()
+
         self.api_key = 'VjAEDf7KZQF5WfSHJjuwz7HaEcbAkFdpQovrtf8S'
-        self.base_url = 'https://api.govinfo.gov/'
+        self.root_url = 'https://api.govinfo.gov/' # this is unnecessary - should use logic from client for relative urls, params
+        self.wait = wait
+
+        self.retry_limit = retry_limit
+
+    @staticmethod
+    def validate_response(response: httpx.Response):
+        return response.status_code == 200
+    
+    async def get(self, url):
+        request_counter = 0
+        while self.retry_limit is False or request_counter < self.retry_limit:
+            response = await super().get(url=url)
+            request_counter += 1
+
+            if 'OVER_RATE_LIMIT' in response.text:
+                if self.wait is True:
+                    asyncio.sleep(self.wait)
+                else:
+                    raise OverRateLimit
+            if self.validate_response(response=response):
+                break
+            
+        # return response and some response metadata (could be dataclass)
+
+        return response
